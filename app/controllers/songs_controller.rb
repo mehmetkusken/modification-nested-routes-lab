3cd8e752d2ca781136d4d299bd43d73a2548ab25
Mehmet <mehmetkusken@gmail.com>
2021-07-07 16:15:46 -0600

    commit tree fc7b3e0fbf065c05c7f158f7203ee842b49a1c8a

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,7 +11,6 @@ class SongsController < ApplicationController
       @songs = Song.all
     end
   end
-
   def show
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
@@ -26,6 +25,11 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -39,32 +43,39 @@ class SongsController < ApplicationController
   end
 
   def edit
+    if params[:artist_id]
+      artist =  Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song not found" if @song.nil?
+      end
+    else
     @song = Song.find(params[:id])
   end
+  end
+
 
   def update
     @song = Song.find(params[:id])
-
     @song.update(song_params)
-
     if @song.save
       redirect_to @song
     else
       render :edit
     end
   end
-
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
-
   private
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_id)
   end
 end
-
